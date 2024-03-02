d258217348d79d23131df230e1828228cb7d6234
Rob Garner <rgarner011235@gmail.com>
2024-03-01 20:39:53 -0700

    Initial commit

---
+++ NavigationCorrection.ino
@@ -0,0 +1,54 @@
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_LIS3MDL.h>
+#include <Adafruit_APDS9960.h> 
+
+//Instantiate our sensor classes
+Adafruit_APDS9960 apds9960; // proximity, gesture
+Adafruit_LIS3MDL lis3mdl;  
+
+//Variables to store sensor values
+float magnetic_x, magnetic_y, magnetic_z;
+uint8_t proximity;
+
+void setup(void) 
+{
+  Serial.begin(115200);
+  Serial.println("Magnetometer Test"); Serial.println("");
+  
+  // initialize the sensors
+  apds9960.begin();
+  apds9960.enableProximity(true);
+  lis3mdl.begin_I2C();
+}
+
+void loop(void) 
+{
+    //get promimity/gesture readings
+  proximity = apds9960.readProximity();
+
+  /* Get a new sensor event */ 
+  // sensors_event_t event; 
+  // mag.getEvent(&event);
+  //get magnetometer readings
+  lis3mdl.read();
+  magnetic_x = lis3mdl.x;
+  magnetic_y = lis3mdl.y;
+  magnetic_z = lis3mdl.z;
+
+  float Pi = 3.14159;
+  
+  // Calculate the angle of the vector y,x
+  float heading = (atan2(magnetic_z,magnetic_y) * 180) / Pi;
+  
+  // Normalize to 0-360
+  if (heading < 0)
+  {
+    heading = 360 + heading;
+  }
+  //Output data as comma seperated values with heading first and proximity second.
+  Serial.print(heading);
+  Serial.print(", ");
+  Serial.println(proximity);
+  delay(500);
+}
